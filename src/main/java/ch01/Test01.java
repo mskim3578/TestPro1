c263b2dd94cd7af5737605adce043a8a95631fba
mskim3578 <mskim3578@naver.com>
2024-03-05 10:24:20 +0900

    tttt

--- src/main/java/ch01/Test01.java
+++ src/main/java/ch01/Test01.java
@@ -4,7 +4,7 @@ public class Test01 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("============123");
+		System.out.println("============123444444444");
 	}
 
 }
